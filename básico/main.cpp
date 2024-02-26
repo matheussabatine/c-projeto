3da3e3b0172d4c91d535c1a5a162d05f2765398e
matheussabatine <70068237+matheussabatine@users.noreply.github.com>
2024-02-26 16:28:15 -0300

    Add files via upload

--- básico/main.cpp
+++ básico/main.cpp
@@ -1,32 +1,59 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
 
 // boolenos:
 // retorno 0(zero) é falso
 // retorno 1 é verdadeiro
 
 int main(){
-
-    std::cout << "Bem-Vindo!" << std::endl;
-
-    const int NUM = 9;
-
-
+    float teste = 561.4f;
+    srand(time(0));
+    const int NUM = rand()%100;
+    int tentativas;
+    int max_tentativas;
     int chute;
-    std::cout << "digite o chute: ";
-    std::cin >> chute;
+    bool resposta_correta = false;
+    bool resposta_maior = false;
 
-    bool resposta_correta = chute == NUM;
-    bool resposta_maior = chute > NUM;
+    std::cout << "Bem-Vindo! " << NUM << std::endl;
+    std::cout << "Digite o máximo de tentativas: ";
+    std::cin >> max_tentativas;
+
+    
+
+
+    for(tentativas = 1; tentativas <= max_tentativas; tentativas++){
+        std::cout << "tentativa " << tentativas << std::endl;
+        std::cout << "digite o chute: ";
+        std::cin >> chute;
+
+        resposta_correta = chute == NUM;
+        resposta_maior = chute > NUM;
+
+        if(resposta_correta){
+            std::cout << "resposta correta" << std::endl;
+            break;
+
+        }
+        else if(resposta_maior){
+            std::cout << "o número é menor do que " << chute << std::endl;
+        }
+        else{
+            std::cout << "o número é maior do que " << chute << std::endl;
+        }
+
+    }
     if(resposta_correta){
-        std::cout << "resposta correta" << std::endl;
+        std::cout << "Vitória em " << tentativas << " tentativas"<< std::endl;
+    }
 
-    }
-    else if(resposta_maior){
-        std::cout << "o número é menor do que " << chute << std::endl;
-    }
-    else{
-        std::cout << "o número é maior do que " << chute << std::endl;
-    }
+    std::cout << "GAME OVER" << std::endl;
+    //teste
+    std::cout << std::fixed << std::setprecision(2) << teste << std::endl;
+    
     
     return 0;
 }
